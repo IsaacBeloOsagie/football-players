af9c1abf2875ed0126585947278b96e5836234e1
Isaac <thierryisthebest1414@gmail.com>
2024-05-17 01:29:43 +0100

    PlayerController updates

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -7,46 +7,51 @@ use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-    /**
+    /**crud >>> create, read, update, delete
      * Display a listing of the resource.
+     * Http method : GET
      */
     public function index()
     {
-        return Player::all();
+        return Player::all();  //read
     }
 
     /**
      * Store a newly created resource in storage.
+     *  Http method: POST and BODY_PLAYER_INFO.
      */
     public function store(Request $request)
     {
-        $player = Player::create($request->all());
+        $player = Player::create($request->all()); //create
         return response()->json($player, 201);
     }
 
     /**
      * Display the specified resource.
+     * Http method: GET (id)
      */
     public function show(Player $player)
     {
-        return $player;
+        return $player; //read
     }
 
     /**
      * Update the specified resource in storage.
+     * Http method: POST (id) and BODY_PLAYER_INFORMATION
      */
     public function update(Request $request, Player $player)
     {
-        $player->update($request->all());
-        return response()->json($player, 200);
+        $player->update($request->all()); //update
+        return response()->json($player, 200); 
     }
 
     /**
      * Remove the specified resource from storage.
+     * Http method : POST (id)
      */
     public function destroy(Player $player)
     {
-        $player->delete();
+        $player->delete(); //delete
         return response()->json(null, 204);
     }
 }
